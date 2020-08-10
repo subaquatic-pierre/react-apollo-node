6530301d7d877037c32f8113aa07df91e7a27f44
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-08-10 08:01:26 +0400

    completed reipce integration test

--- client/src/components/Recipe/__tests__/Recipe.js
+++ client/src/components/Recipe/__tests__/Recipe.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 import Recipe from '../Recipe';
-import { render, wait, fireEvent } from '../../../test/utils'
+import { render, wait } from '../../../test/utils'
 import { recipe1, user } from '../../../test/__mocks__'
-import * as mockHelpers from '../../../updateCache/helpers'
 import mockGetUser from '../../../auth/getUser'
 
 jest.mock('../../../updateCache/updateUserFavs')
@@ -95,9 +94,3 @@ describe('basic rendering', () => {
         expect(await (await findByLabelText(/like-button/i)).textContent).toContain('Liked')
     })
 })
-
-
-
-
-
-
