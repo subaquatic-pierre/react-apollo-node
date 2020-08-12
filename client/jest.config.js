1677622cce4193879b387afdb2312e682a481acc
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-08-12 22:04:05 +0400

    before jest coverage settings

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,55 +1,12 @@
-const config = require('kcd-scripts/jest')
-
 module.exports = {
-  ...config,
-  setupFilesAfterEnv: ['src/test/setupEnv.js']
+  // ...require('./test/jest-common'),
+  testPathIgnorePatterns: ['<rootDir>/server/', '<rootDir>/src/mutations/'],
+  coverageThreshold: {
+    global: {
+      statements: 15,
+      branches: 10,
+      functions: 15,
+      lines: 15,
+    },
+  },
 }
-
-
-// "use strict";
-
-// const path = require('path');
-
-// const {
-//   ifAnyDep,
-//   hasFile,
-//   hasPkgProp,
-//   fromRoot
-// } = require('../utils');
-
-// const here = p => path.join(__dirname, p);
-
-// const useBuiltInBabelConfig = !hasFile('.babelrc') && !hasPkgProp('babel');
-// const ignores = ['/node_modules/', '/__fixtures__/', '/fixtures/', '/__tests__/helpers/', '/__tests__/utils/', '__mocks__'];
-// const jestConfig = {
-//   roots: [fromRoot('src')],
-//   testEnvironment: ifAnyDep(['webpack', 'rollup', 'react', 'preact'], 'jsdom', 'node'),
-//   testURL: 'http://localhost',
-//   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
-//   collectCoverageFrom: ['src/**/*.+(js|jsx|ts|tsx)'],
-//   testMatch: ['**/__tests__/**/*.+(js|jsx|ts|tsx)'],
-//   testPathIgnorePatterns: [...ignores],
-//   coveragePathIgnorePatterns: [...ignores, 'src/(umd|cjs|esm)-entry.js$'],
-//   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
-//   coverageThreshold: {
-//     global: {
-//       branches: 100,
-//       functions: 100,
-//       lines: 100,
-//       statements: 100
-//     }
-//   },
-//   watchPlugins: [require.resolve('jest-watch-typeahead/filename'), require.resolve('jest-watch-typeahead/testname')]
-// };
-
-// if (hasFile('tests/setup-env.js')) {
-//   jestConfig.setupFilesAfterEnv = [fromRoot('tests/setup-env.js')];
-// }
-
-// if (useBuiltInBabelConfig) {
-//   jestConfig.transform = {
-//     '^.+\\.(js|jsx|ts|tsx)$': here('./babel-transform')
-//   };
-// }
-
-// module.exports = jestConfig;
